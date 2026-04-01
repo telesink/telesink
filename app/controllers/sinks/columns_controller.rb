a68846f6c483f0c1737facc3fd81b65dd74254d7
Kyrylo Silin <silin@kyrylo.org>
2026-04-02 00:15:58 +0800

    temporarily skip demo restrictions for columns

--- app/controllers/sinks/columns_controller.rb
+++ app/controllers/sinks/columns_controller.rb
@@ -1,7 +1,7 @@
 class Sinks::ColumnsController < ApplicationController
   include SinkScoped
 
-  skip_demo_restrictions only: %i[show]
+  skip_demo_restrictions # only: %i[show]
 
   before_action :set_column, only: %i[edit update show destroy]
 
