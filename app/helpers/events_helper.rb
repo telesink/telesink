201c21b6975020a4d7604fae41831e1bb8ace008
Kyrylo Silin <silin@kyrylo.org>
2026-04-02 10:20:28 +0800

    don't truncate <mark>, which breaks highlighting

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,11 +1,17 @@
 module EventsHelper
   def highlighted_event_body(event, column, length: 87)
-    text = if column&.search_term.present?
-      highlight(event.text, column.search_term, highlighter: '<mark class="event-preview__highlight">\1</mark>')
-    else
-      event.text
-    end
+    text = event.text
 
-    truncate(text, length: length, omission: "…").html_safe
+    if column&.search_term.present?
+      truncated = truncate(text, length: length, omission: "…")
+
+      highlight(
+        truncated,
+        column.search_term,
+        highlighter: '<mark class="event-preview__highlight">\1</mark>'
+      )
+    else
+      truncate(text, length: length, omission: "…")
+    end
   end
 end
